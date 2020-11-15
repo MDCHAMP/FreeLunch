6133ab9781eab64bc94078df10b35bf16493b1cc
MDCHAMP <max.champneys@gmail.com>
2020-11-15 19:20:28 +0000

    configure setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='opt2',
+    version='0.0.1',
+    description='Meta-heuristic optimisation tools',
+    py_modules=['opt2'],
+    package_dir={'':'src'},
+)
+
