8f14dfa37c760d9e83e0e85c8903838442e466d0
MDCHAMP <max.champneys@gmail.com>
2020-11-18 11:15:17 +0000

    versioning

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('README.md') as f:
 # This call to setup() does all the work
 setup(
     name="freelunch",
-    version="0.0.3",
+    version="0.0.4",
     description="Heuristic and meta-heuristic optimisation suite in Python",
     long_description=rm,
     long_description_content_type="text/markdown",
