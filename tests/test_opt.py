279b096fb45908fa18f0aede6a3542e5bb959777
MDCHAMP <max.champneys@gmail.com>
2021-01-19 15:46:46 +0000

    added test for odered solutions in full output

--- tests/test_opt.py
+++ tests/test_opt.py
@@ -30,5 +30,6 @@ def test_run_opt_with_defualts(opt, obj, n):
 	o = obj(n)
 	np.random.seed(1)
 	out = opt(obj=o, bounds=o.bounds)(full_output=True)
-	assert type(out) is dict
-	assert (obj.tol is None) or (abs(out['scores'][0] - obj.f0) < obj.tol)
+	assert type(out) is dict # returns dict
+	assert all(x<=y for x, y in zip(out['scores'], out['scores'][1:])) # scores are ordered
+	assert (obj.tol is None) or (abs(out['scores'][0] - obj.f0) < obj.tol) # within tolerance
