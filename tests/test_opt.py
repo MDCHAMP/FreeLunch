4fbc65d8ec39f0ab8311fec05bf452703ef004ea
MDCHAMP <max.champneys@gmail.com>
2021-02-06 16:14:56 +0000

    benchmark performance

--- tests/test_opt.py
+++ tests/test_opt.py
@@ -29,8 +29,24 @@ def test_true_optima(obj, n):
 @pytest.mark.parametrize('n', dims)
 def test_run_opt_with_defualts(opt, obj, n):
 	o = obj(n)
-
 	out = opt(obj=o, bounds=o.bounds)(full_output=True)
 	assert type(out) is dict # returns dict
 	assert all(x<=y for x, y in zip(out['scores'], out['scores'][1:])) # scores are ordered
 	assert (obj.tol is None) or (abs(out['scores'][0] - obj.f0) < obj.tol) # within tolerance
+
+
+# Do you even lift bro?
+
+@pytest.mark.benchmark(group="Optimisers")
+@pytest.mark.parametrize('opt', optimiser_classes)
+def test_bench_optimisers(benchmark, opt):
+	o = exponential(1)
+	bench_opt = opt(obj=o, bounds=o.bounds)
+	benchmark(bench_opt, return_m=1)
+
+@pytest.mark.benchmark(group="Benchmarks")
+@pytest.mark.parametrize('obj', benchmark_problems)
+def test_bench_benchmarks(benchmark, obj):
+	o = obj(2)
+	optima = benchmark(o, o.optimum)
+	assert  optima == o.f0
