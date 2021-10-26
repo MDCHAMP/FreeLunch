d60a1749e031171a0a51421c5c8f67199b2d6f5b
MDCHAMP <max.champneys@gmail.com>
2021-10-26 16:27:32 +0100

    v0.0.11

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('README.md') as f:
 # This call to setup() does all the work
 setup(
     name="freelunch",
-    version="0.0.10",
+    version="0.0.11",
     description="Heuristic and meta-heuristic optimisation suite in Python",
     long_description=rm,
     long_description_content_type="text/markdown",
