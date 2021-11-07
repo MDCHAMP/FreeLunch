1dcd30a7bab154bb49e1a3fccfbe8179e028e24d
MDCHAMP <max.champneys@gmail.com>
2021-11-07 23:02:59 +0000

    update testing

--- tests/test_optimisers.py
+++ tests/test_optimisers.py
@@ -3,82 +3,91 @@ Testing the optimisers
 
 Testing for function not performance see benchmarking script
 '''
+from freelunch.benchmarks import exponential
+from freelunch import DE, SA, PSO, QPSO, SADE, KrillHerd, SA
 import pytest
 import numpy as np
 import json
 np.random.seed(100)
 
 
-from freelunch import DE, SA, PSO, QPSO, SADE, KrillHerd, SA
-from freelunch.benchmarks import exponential
-
 optimiser_classes = [SA, DE, PSO, QPSO, SADE, KrillHerd]
-dims = [1,2,3]
+dims = [1, 2, 3]
+
 
 def set_testing_hypers(opt):
     hypers = opt.hyper_defaults
     hypers['N'] = 5
     hypers['G'] = 2
-    hypers['K'] = 2 # SA should really use G as well...
-    
+    hypers['K'] = 2  # SA should really use G as well...
+
     return hypers
 
 
 @pytest.mark.parametrize('opt', optimiser_classes)
 def test_instancing_defaults(opt):
-	o = opt()
-	assert o.hypers == opt.hyper_defaults
-    
+    o = opt()
+    for k, v in o.hypers.items():
+        if k in opt.hyper_defaults:
+            assert np.all(v == opt.hyper_defaults[k])
+
 # Since this happens in the base class it should be ok to just test DE
-@pytest.mark.parametrize('n', [1,3,5])
+
+
+@pytest.mark.parametrize('n', [1, 3, 5])
 def test_nfe(n):
     o = exponential(n)
     hypers = set_testing_hypers(DE)
     out = DE(obj=o, bounds=o.bounds, hypers=hypers)(nruns=n, full_output=True)
-    assert out['nfe'] == (out['hypers']['G'] + 1) * out['hypers']['N'] * n 
+    assert out['nfe'] == (out['hypers']['G'] + 1) * out['hypers']['N'] * n
+
 
 @pytest.mark.parametrize('opt', optimiser_classes)
-@pytest.mark.parametrize('n', [1,3])
-@pytest.mark.parametrize('d', [1,3,5])
-def test_run(opt,n,d):
+@pytest.mark.parametrize('n', [1, 3])
+@pytest.mark.parametrize('d', [1, 3, 5])
+def test_run(opt, n, d):
     o = exponential(d)
     hypers = set_testing_hypers(opt)
     out = opt(obj=o, bounds=o.bounds, hypers=hypers)(nruns=n, full_output=True)
-    assert(len(out['solutions'])==n*hypers['N'])
-    assert(all(x<=y for x, y in zip(out['scores'], out['scores'][1:]))) # scores are ordered
+    assert(len(out['solutions']) == n*hypers['N'])
+    # scores are ordered
+    assert(all(x <= y for x, y in zip(out['scores'], out['scores'][1:])))
     for o in out['solutions']:
-        for i,v in enumerate(o):
-            assert(v > out['bounds']['bounds'][i][0])
-            assert(v < out['bounds']['bounds'][i][1])
+        for i, v in enumerate(o):
+            assert(v > out['bounds'][i][0])
+            assert(v < out['bounds'][i][1])
 
 
 @pytest.mark.parametrize('opt', optimiser_classes)
-@pytest.mark.parametrize('d', [1,3,5])
-def test_run_one(opt,d):
+@pytest.mark.parametrize('d', [1, 3, 5])
+def test_run_one(opt, d):
     o = exponential(d)
     hypers = set_testing_hypers(opt)
     out = opt(obj=o, bounds=o.bounds, hypers=hypers)(full_output=True)
-    assert(len(out['solutions'])==hypers['N'])
+    assert(len(out['solutions']) == hypers['N'])
 
 
 @pytest.mark.parametrize('opt', optimiser_classes)
-@pytest.mark.parametrize('n', [1,3])
-@pytest.mark.parametrize('m', [1,3])
-@pytest.mark.parametrize('d', [1,5])
-def test_run_not_full(opt,n,m,d):
+@pytest.mark.parametrize('n', [1, 3])
+@pytest.mark.parametrize('m', [1, 3])
+@pytest.mark.parametrize('d', [1, 5])
+def test_run_not_full(opt, n, m, d):
     o = exponential(d)
     hypers = set_testing_hypers(opt)
-    out = opt(obj=o, bounds=o.bounds, hypers=hypers)(nruns=n, return_m=m, full_output=False)
-    assert(len(out)==m)
+    out = opt(obj=o, bounds=o.bounds, hypers=hypers)(
+        nruns=n, return_m=m, full_output=False)
+    assert(len(out) == m)
     assert([(len(s) == d) for s in out])
 
+
 @pytest.mark.parametrize('opt', optimiser_classes)
-@pytest.mark.parametrize('n', [1,3])
-def test_can_json(opt,n):
+@pytest.mark.parametrize('n', [1, 3])
+def test_can_json(opt, n):
     o = exponential(1)
     hypers = set_testing_hypers(opt)
     out = opt(obj=o, bounds=o.bounds, hypers=hypers)(nruns=n, full_output=True)
     s = json.dumps(out)
+
 
 @pytest.mark.parametrize('opt', optimiser_classes)
 def test_repr(opt):
--- tests/test_tech.py
+++ tests/test_tech.py
@@ -1,112 +1,104 @@
 '''
 Testing tech
 '''
+from freelunch.zoo import animal, particle, krill
+from freelunch.tech import *
 import pytest
 import numpy as np
 
 np.random.seed(100)
 
-from freelunch.tech import *
-from freelunch.zoo import animal, particle, krill
 
-animals = [None, animal,particle, krill]
+animals = [None, animal, particle, krill]
+
 
 def dummy_obj_fun(dna):
     return 1
 
-@pytest.mark.parametrize('N',[1,3])
-@pytest.mark.parametrize('dim',[1,3])
-@pytest.mark.parametrize('creature',animals)
+
+def bound_fixture(a, b, bnds, bounder, **hypers):
+    a = animal(dna=a)
+    bounder(a, bnds, **hypers)
+    assert np.all(a.dna == b)
+
+
+@pytest.mark.parametrize('N', [1, 3])
+@pytest.mark.parametrize('dim', [1, 3])
+@pytest.mark.parametrize('creature', animals)
 def test_uniform_init(N, dim, creature):
 
-    bounds = np.empty((dim,2))
+    bounds = np.empty((dim, 2))
     for n in range(dim):
-        bounds[n,1] = 10*np.random.rand()
-        bounds[n,0] = -bounds[n,1]
-    
-    
+        bounds[n, 1] = 10*np.random.rand()
+        bounds[n, 0] = -bounds[n, 1]
+
     if creature is None:
         pop = uniform_continuous_init(bounds, N)
     else:
         pop = uniform_continuous_init(bounds, N, creature=creature)
 
     assert(len(pop) == N)
-    assert(np.all(pop[0].dna > bounds[:,0]))
-    assert(np.all(pop[0].dna < bounds[:,1]))
+    assert(np.all(pop[0].dna > bounds[:, 0]))
+    assert(np.all(pop[0].dna < bounds[:, 1]))
 
 
 def test_compute_obj():
 
     N = 10
-    pop = uniform_continuous_init(np.array([[-1.,1.]]),N)
+    pop = uniform_continuous_init(np.array([[-1., 1.]]), N)
     pop = compute_obj(pop, dummy_obj_fun)
 
     assert(len(pop) == N)
-    for p in pop: assert(p.fitness == 1)
+    for p in pop:
+        assert(p.fitness == 1)
+
+
+def test_sticky_bounds():
+    bounds = [[-1, 1], [-2, 2]]
+    eps = 1e-12
+    bound_fixture([0.5, 1.2], [0.5, 1.2], bounds, sticky_bounds, eps=eps)
+    bound_fixture([-2, 1.2],  [-1+eps, 1.2],bounds, sticky_bounds, eps=eps)
+    bound_fixture([0.5, 2.2], [0.5, 2-eps],bounds, sticky_bounds, eps=eps)
+    bound_fixture([-1.5, 2.2], [-1+eps, 2-eps],bounds, sticky_bounds, eps=eps)
+    bound_fixture([1.5, -2.2], [1-eps, -2+eps],bounds, sticky_bounds, eps=eps)
+
+
+def test_bounds_as_mat():
+
+    bounds = [[-1, 1], [-2, 2]]
+    bounds_mat = np.array(bounds)
+    assert(np.all(bounds_as_mat(bounds) == bounds_mat))
+
 
 def test_lin_reduce():
 
-    lims = [2,4]
+    lims = [2, 4]
     n_max = 10
-    assert(lin_reduce(lims,0,n_max) == 4)
-    assert(lin_reduce(lims,n_max,n_max) == 2)
-    assert(lin_reduce(lims,5,n_max) == 3)
+    assert(lin_reduce(lims, 0, n_max) == 4)
+    assert(lin_reduce(lims, n_max, n_max) == 2)
+    assert(lin_reduce(lims, 5, n_max) == 3)
 
-    lims = [4,2]
+    lims = [4, 2]
     n_max = 10
-    assert(lin_reduce(lims,0,n_max) == 4)
-    assert(lin_reduce(lims,n_max,n_max) == 2)
-    assert(lin_reduce(lims,5,n_max) == 3)
+    assert(lin_reduce(lims, 0, n_max) == 4)
+    assert(lin_reduce(lims, n_max, n_max) == 2)
+    assert(lin_reduce(lims, 5, n_max) == 3)
+
 
 def test_pdist():
 
-    A = np.array([[1,2],[3,4]])
-    B = np.array([[5,6],[7,8]])
+    A = np.array([[1, 2], [3, 4]])
+    B = np.array([[5, 6], [7, 8]])
 
-    DAA = np.empty((2,2))
-    DAB = np.empty((2,2))
+    DAA = np.empty((2, 2))
+    DAB = np.empty((2, 2))
 
     # Slow lame euclidean distance to check big brain
     for i in range(2):
         for j in range(2):
-            DAA[i,j] = np.sqrt(np.sum((A[i,:] - A[j,:])**2))
-            DAB[i,j] = np.sqrt(np.sum((A[i,:] - B[j,:])**2))
+            DAA[i, j] = np.sqrt(np.sum((A[i, :] - A[j, :])**2))
+            DAB[i, j] = np.sqrt(np.sum((A[i, :] - B[j, :])**2))
 
     assert(np.all(pdist(A) == DAA))
-    assert(np.all(pdist(A,A) == DAA))
-    assert(np.all(pdist(A,B) == DAB))
-
-
-bounders = [StickyBounds]
-@pytest.mark.parametrize('dim',[1,3])
-@pytest.mark.parametrize('bounding_strat', bounders)
-def test_bounder(dim, bounding_strat):
-
-    bounds = np.empty((dim,2))
-    for n in range(dim):
-        bounds[n,1] = 10*np.random.rand()
-        bounds[n,0] = -bounds[n,1]
-    
-    pop = uniform_continuous_init(bounds, 5)
-
-    B = Bounder(bounds)
-    with pytest.raises(NotImplementedError):
-        B(pop)
-
-    B = bounding_strat(bounds)
-    pop_old = pop.copy()
-    assert(all([n == o for o,n in zip(pop_old, pop)]))
-
-    for p in pop:
-        if np.random.uniform() < 0.5:
-            p.dna = bounds[:,0] - 1
-        else:
-            p.dna = bounds[:,1] + 1
-
-    B(pop)
-    assert(all([(np.all(p.dna == bounds[:,0]+1e-12) or np.all(p.dna == bounds[:,1]-1e-12)) for p in pop]))
-
-    B = NoBounds()
-    with pytest.raises(Warning):
-        B(pop)
-    
+    assert(np.all(pdist(A, A) == DAA))
+    assert(np.all(pdist(A, B) == DAB))
